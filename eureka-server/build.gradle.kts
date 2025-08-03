9bd349addf164f7a44b1b5a69109e997deef89f2
Divyesh Mangla <174847645+DivyeshMangla@users.noreply.github.com>
2025-08-03 18:37:51 +0530

    Initial Commit

---
+++ eureka-server/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("org.springframework.boot") version "3.3.2"
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+}
